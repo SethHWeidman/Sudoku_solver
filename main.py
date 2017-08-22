ac6c36495657ead14dd7fa69c0d26a969b986923
Seth Weidman <sethhweidman@gmail.com>
2017-08-22 16:57:35 -0500

    Added comments throughout, borders

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import time
 app = Flask(__name__)
 
 def try_read_int(cell, default_value='123456789'):
+    '''
+    This function is applied to every cell, from A1 through I9.
+    It takes in a cell and tries to return a string representing the value of
+    that cell. Otherwise I return a default value.
+    '''
     acceptable_values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     try:
         value = int(request.form[cell])
-        # if value in acceptable_values:
         return str(value)
-        # else:
-        #     "Please enter an integer between 0 and 9"
     except:
         return default_value
 '''
@@ -23,9 +25,10 @@ ROWS = 'ABCDEFGHI'
 COLS = '123456789'
 boxes = get_boxes(ROWS, COLS)
 
-
 @app.route('/')
 def hello_world():
+    '''Sets up the Sudoku with an easy-to-solve puzzle.
+    Uses Javascript to render it.'''
     easy_dict = {"A1": 7, "A5": 9,
                  "B2": 6, "B3": 8, "B8": 3, "B9": 1,
                  "C1": 5, "C3": 1, "C4": 3, "C7": 4, "C8": 2, "C9": 7,
@@ -37,8 +40,15 @@ def hello_world():
                  "I5": 2, "I9": 5}
     return render_template('index.html', input_sudoku=easy_dict)
 
+
 @app.route('/sudoku_action/', methods=['GET', 'POST'])
 def sudoku_action():
+    '''
+    This function:
+    -Loops through the boxes and uses "try_read_int" to fill them with values.
+    -If we have requested to "solve the whole puzzle", then solve the whole puzzle.
+    -Otherwise, simply return the template with the same values.
+    '''
     cell_data = {}
     for box in boxes:
         cell_data[box] = ''
@@ -52,17 +62,16 @@ def sudoku_action():
 
 @app.route('/eliminate_one/', methods=['GET', 'POST'])
 def update_one():
+    '''
+    Update just one value in the cell.
+    '''
     cell_data = {}
     for box in boxes:
         cell_data[box] = ''
         cell_data[box] = try_read_int(box)
     new_values, message, peer = eliminate_one(cell_data)
-    time.sleep(6)
-    # import pdb; pdb.set_trace()
+    time.sleep(3)
     return jsonify(values=new_values, message=message, peer=peer)
-
-# @app.route('/solve_sudoku_new/', methods=['GET', 'POST'])
-# def solve_sudoku():
 
 
 
